38a739bbe88a1d7cffb5e228fd81541f6ef04fe4
Sean Davis <sean.dev.davis@gmail.com>
2022-05-13 14:31:41 -0500

    added songs controller and get method

---
+++ IMusicDB-API/Controllers/SongsController.cs
@@ -0,0 +1,24 @@
+﻿using IMusicDB_API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace IMusicDB_API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SongsController : ControllerBase
+    {
+        private List<Song> songs = new List<Song>()
+        {
+            new Song() {Id = 0, Title = "We Gonna Make It", Language = "English"},
+            new Song() {Id = 0, Title = "Win Or Lose", Language = "English"}
+        };
+
+        [HttpGet]
+        public IEnumerable<Song> Get()
+        {
+            return songs;
+        }
+    }
+}
